1a089d683213500be8a4b5c259cb95e75329de1f
Vinicius Carvalho Machado <viniciuscmac@gmail.com>
2020-09-19 10:03:00 -0300

    First commit, configuring environment

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card, CardHeader, CardBody } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card>
+        <CardHeader>
+          Calendar filters will be here
+        </CardHeader>
+        <CardBody>
+          Here it comes the calendar component
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
